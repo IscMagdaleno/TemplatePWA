2b1deaba5de5e66a742466c8cb389d7e5663f869
Magdaleno Martínez Unzueta <iscmagdaleno@gmail.com>
2025-06-10 08:18:58 -0600

    Limpieza de la plantilla

--- TemplatePWA/TemplatePWA.PWA/Areas/TestModul/Components/TableTestTable.razor.cs
+++ TemplatePWA/TemplatePWA.PWA/Areas/TestModul/Components/TableTestTable.razor.cs
@@ -1,12 +1,9 @@
 ﻿using EngramaCoreStandar.Dapper.Results;
 using EngramaCoreStandar.Extensions;
-using EngramaCoreStandar.Logger;
 
 using Microsoft.AspNetCore.Components;
 
 using MudBlazor;
-
-using System.Reflection;
 
 using TemplatePWA.PWA.Areas.TestModul.Utiles;
 using TemplatePWA.PWA.Helpers;
@@ -15,23 +12,17 @@ namespace TemplatePWA.PWA.Areas.TestModul.Components
 {
 	public partial class TableTestTable
 	{
-
-		[Inject] private ILoggerHelper _LoggerHelper { get; set; }
-		private string GetPath() => GetType().ToString() + " - ";
 		[Inject] private LoadingState Loading { get; set; }
 		[Inject] private ISnackbar Snackbar { get; set; }
-
 		[Parameter] public DataTest Data { get; set; }
 
 		protected override void OnInitialized()
 		{
-			_LoggerHelper.Info($"{GetPath()}{MethodBase.GetCurrentMethod().Name}");
 
 		}
 
 		protected override async Task OnInitializedAsync()
 		{
-			_LoggerHelper.Info($"{GetPath()}{MethodBase.GetCurrentMethod().Name}");
 			Loading.Show();
 			ShowSnake(await Data.PostTestTable());
 			Loading.Hide();
--- TemplatePWA/TemplatePWA.PWA/Areas/TestModul/PageTestModule.razor.cs
+++ TemplatePWA/TemplatePWA.PWA/Areas/TestModul/PageTestModule.razor.cs
@@ -1,10 +1,7 @@
-﻿using EngramaCoreStandar.Logger;
-using EngramaCoreStandar.Mapper;
+﻿using EngramaCoreStandar.Mapper;
 using EngramaCoreStandar.Servicios;
 
 using Microsoft.AspNetCore.Components;
-
-using System.Reflection;
 
 using TemplatePWA.PWA.Areas.TestModul.Utiles;
 using TemplatePWA.PWA.Helpers;
@@ -14,24 +11,19 @@ namespace TemplatePWA.PWA.Areas.TestModul
 	public partial class PageTestModule
 	{
 		[Inject] private IValidaServicioService validaServicioService { get; set; }
-
 		[Inject] private IHttpService httpService { get; set; }
 		[Inject] private MapperHelper mapperHelper { get; set; }
-		[Inject] private ILoggerHelper _LoggerHelper { get; set; }
-		private string GetPath() => GetType().ToString() + " - ";
 		[Inject] private LoadingState Loading { get; set; }
 
 		public DataTest Data { get; set; }
 
 		protected override void OnInitialized()
 		{
-			_LoggerHelper.Info($"{GetPath()}{MethodBase.GetCurrentMethod().Name}");
 			Data = new DataTest(httpService, mapperHelper, validaServicioService);
 		}
 
 		protected override async Task OnInitializedAsync()
 		{
-			_LoggerHelper.Info($"{GetPath()}{MethodBase.GetCurrentMethod().Name}");
 		}
 
 
